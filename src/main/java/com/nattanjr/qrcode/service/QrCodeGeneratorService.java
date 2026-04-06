f652accfe3be5c7d6fad0f5f3aa25ecaaeb48a66
Nattan <fnatanieljunior@gmail.com>
2026-04-06 18:34:14 -0300

    Feature: QR code service created

---
+++ src/main/java/com/nattanjr/qrcode/service/QrCodeGeneratorService.java
@@ -0,0 +1,40 @@
+package com.nattanjr.qrcode.service;
+
+import com.google.zxing.BarcodeFormat;
+import com.google.zxing.WriterException;
+import com.google.zxing.client.j2se.MatrixToImageWriter;
+import com.google.zxing.common.BitMatrix;
+import com.google.zxing.qrcode.QRCodeWriter;
+import com.nattanjr.qrcode.dtos.QrCodeGenerateResponse;
+import com.nattanjr.qrcode.ports.StoragePort;
+import org.apache.tomcat.util.http.fileupload.ByteArrayOutputStream;
+import org.springframework.stereotype.Service;
+
+import java.io.IOException;
+import java.util.UUID;
+
+@Service
+public class QrCodeGeneratorService {
+    private final StoragePort storage;
+
+    public QrCodeGeneratorService(StoragePort storage) {
+        this.storage = storage;
+    }
+
+    public QrCodeGenerateResponse generateAndUploadQrCode(String text) throws IOException, WriterException {
+        QRCodeWriter qrCodeWriter = new QRCodeWriter();
+        BitMatrix bitMatrix = qrCodeWriter.encode(text, BarcodeFormat.QR_CODE, 200, 200);
+
+        ByteArrayOutputStream pngOutputStream = new ByteArrayOutputStream();
+        MatrixToImageWriter.writeToStream(bitMatrix, "PNG", pngOutputStream);
+
+        byte[] pngQrCOdeData = pngOutputStream.toByteArray();
+
+
+        String url = storage.uploadFile(pngQrCOdeData, UUID.randomUUID().toString(), "image/png");
+
+        return new QrCodeGenerateResponse(url);
+
+    }
+
+}
