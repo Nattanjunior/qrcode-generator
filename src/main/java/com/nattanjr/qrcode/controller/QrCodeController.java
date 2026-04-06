1dd5296c40fa5272a90e0d710ce18243190bdf29
Nattan <fnatanieljunior@gmail.com>
2026-04-06 20:27:24 -0300

    Feature: Added to the controller, the QR code service.

--- src/main/java/com/nattanjr/qrcode/controller/QrCodeController.java
+++ src/main/java/com/nattanjr/qrcode/controller/QrCodeController.java
@@ -1,6 +1,7 @@
 package com.nattanjr.qrcode.controller;
 import com.nattanjr.qrcode.dtos.QrCodeGenerateRequest;
 import com.nattanjr.qrcode.dtos.QrCodeGenerateResponse;
+import com.nattanjr.qrcode.service.QrCodeGeneratorService;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
@@ -8,8 +9,19 @@ import org.springframework.web.bind.annotation.*;
 @RequestMapping("/qrcode")
 public class QrCodeController {
 
+    private final QrCodeGeneratorService qrCodeGeneratorService;
+
+    public QrCodeController (QrCodeGeneratorService qrCodeService) {
+        this.qrCodeGeneratorService = qrCodeService;
+    }
+
     @PostMapping
     public ResponseEntity<QrCodeGenerateResponse> generate(@RequestBody QrCodeGenerateRequest request) {
-        return null;
+       try{
+          QrCodeGenerateResponse response = this.qrCodeGeneratorService.generateAndUploadQrCode(request.text());
+          return ResponseEntity.ok(response);
+       }catch(Exception error){
+           return ResponseEntity.internalServerError().build();
+       }
     }
 }
